452287b2fbceca16e5c24698ab135c351baa0016
lesliebinbin <leslie.huang@basf.com>
2026-01-12 22:48:31 +0800

    open with vscode

--- private/packages/buffer-path-utils/buffer-path-utils.el
+++ private/packages/buffer-path-utils/buffer-path-utils.el
@@ -1,13 +1,13 @@
-(defun vscode/buffer-full-path ()
+(defun leslie/buffer-full-path ()
   "Return the full path of the current buffer's file."
   (or (buffer-file-name)))
 
-(defun vscode/buffer-full-lsp-workspace-path ()
+(defun leslie/buffer-full-lsp-workspace-path ()
   (when (and (bound-and-true-p lsp-mode)
              (fboundp 'lsp-workspace-root)))
   (lsp-workspace-root))
 
-(defun vscode/buffer-project-full-path ()
+(defun leslie/buffer-project-full-path ()
   "Return the full path of the current buffer's project."
   (when (fboundp 'project-current)
     (let ((project (project-current)))
