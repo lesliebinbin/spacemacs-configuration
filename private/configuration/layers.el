28e65cc1a86849b25008c26027aafe48a639c44e
U10090080 <leslie.huang@basf.com>
2026-01-13 09:48:10 +0800

    better config

---
+++ private/configuration/layers.el
@@ -0,0 +1,166 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
+(defun dotspacemacs/layers ()
+  "Layer configuration:
+This function should only modify configuration layer settings."
+  (setq-default
+   ;; Base distribution to use. This is a layer contained in the directory
+   ;; `+distribution'. For now available distributions are `spacemacs-base'
+   ;; or `spacemacs'. (default 'spacemacs) dotspacemacs-distribution 'spacemacs
+   ;; Lazy installation of layers (i.e. layers are installed only when a file
+   ;; with a supported type is opened). Possible values are `all', `unused'
+   ;; and `nil'. `unused' will lazy install only unused layers (i.e. layers
+   ;; not listed in variable `dotspacemacs-configuration-layers'), `all' will
+   ;; lazy install any layer that support lazy installation even the layers
+   ;; listed in `dotspacemacs-configuration-layers'. `nil' disable the lazy
+   ;; installation feature and you have to explicitly list a layer in the
+   ;; variable `dotspacemacs-configuration-layers' to install it.
+   ;; (default 'unused) dotspacemacs-enable-lazy-installation 'unused
+   ;; If non-nil then Spacemacs will ask for confirmation before installing
+   ;; a layer lazily. (default t) dotspacemacs-ask-for-lazy-installation t
+   ;; List of additional paths where to look for configuration layers.
+   ;; Paths must have a trailing slash (i.e. "~/.mycontribs/") dotspacemacs-configuration-layer-path '()
+   ;; List of configuration layers to load.
+
+   dotspacemacs-configuration-layers
+
+   `(
+     ;; ----------------------------------------------------------------
+     ;; Example of useful layers you may want to use right away.
+     ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
+     ;; `M-m f e R' (Emacs style) to install them.
+     ;; ----------------------------------------------------------------
+     (auto-completion
+      :variables
+      auto-completion-enable-help-tooltip 'manual
+      auto-completion-return-key-behavior 'complete
+      auto-completion-tab-key-behavior 'cycle
+      auto-completion-enable-snippets-in-popup t
+      auto-completion-use-company-box t
+      :disabled-for git)
+
+     emacs-lisp
+     dap
+     json
+     yaml
+     toml
+     command-log
+     restclient
+     llm-client
+     git
+     helm
+     markdown
+     multiple-cursors
+     (org
+      :variables
+      org-enable-babel-support t
+      org-confirm-babel-evaluate nil
+      org-src-preserve-indentation t
+      org-edit-src-content-indentation 0
+      org-src-fontify-natively t)
+
+     (shell
+      :variables
+      shell-default-height 30
+      shell-default-position 'bottom
+      shell-default-shell 'vterm)
+     syntax-checking
+     version-control
+     semantic
+     (better-defaults
+      :variables
+      better-defaults-move-to-beginning-of-code-first t
+      better-defaults-move-to-end-of-code-first
+      nil)
+     (treemacs
+      :variables
+      treemacs-width 30)
+     csv
+     toml
+     (python
+      :variables
+      python-backend 'lsp
+      python-lsp-server 'pyright
+      python-formatter 'black)
+     javascript
+     typescript
+     vue
+     prettier
+     ,@(unless (custom/apple-intel-p)
+         '(parinfer))
+     (github-copilot
+      :variables
+      copilot-chat-backend 'curl)
+     ,@(when (eq system-type 'darwin)
+         '(
+           (osx :variables
+                osx-command-as 'super
+                osx-option-as 'meta
+                osx-control-as 'control
+                osx-function-as nil
+                osx-right-command-as 'left
+                osx-right-option-as 'left
+                osx-right-control-as 'left
+                osx-swap-option-and-command nil)))
+
+
+
+     ,@(unless (custom/wsl-p)
+         '(
+           (eaf :variables
+                eaf-python-command (getenv "EAF_PYTHON_PATH")
+                eaf-enable-debug nil)))
+
+
+
+
+
+     ,@(when-let* ((conda-home (getenv "CONDA_PREFIX")))
+         `(
+           (conda
+            :variables
+            conda-anaconda-home ,conda-home
+            :config
+            (conda-env-initialize-interactive-shells)
+            (conda-env-initialize-eshell))))
+
+
+
+     (ibuffer :variables ibuffer-group-buffers-by 'projects)
+     (tabs :variables
+           tabs-selected-tab-bar 'over
+           tabs-auto-hide t
+           tabs-auto-hide-delay 3.0))
+
+
+   ;; List of additional packages that will be installed without being wrapped
+   ;; in a layer (generally the packages are installed only and should still be
+   ;; loaded using load/require/use-package in the user-config section below in
+   ;; this file). If you need some configuration for these packages, then
+   ;; consider creating a layer. You can also put the configuration in
+   ;; `dotspacemacs/user-config'. To use a local version of a package, use the
+   ;; `:location' property: '(your-package :location "~/path/to/your-package/")
+   ;; Also include the dependencies as they will not be resolved automatically.
+   dotspacemacs-additional-packages
+   `(
+     jupyter
+     exec-path-from-shell
+     (buffer-path-utils :location ,(expand-file-name "buffer-path-utils" user-packages-directory)))
+
+   ;; A list of packages that cannot be updated.
+   dotspacemacs-frozen-packages
+   '()
+   ;; A list of packages that will not be installed and loaded.
+   dotspacemacs-excluded-packages
+   '(
+     company)
+
+   ;; Defines the behaviour of Spacemacs when installing packages.
+   ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
+   ;; `used-only' installs only explicitly used packages and deletes any unused
+   ;; packages as well as their unused dependencies. `used-but-keep-unused'
+   ;; installs only the used packages but won't delete unused ones. `all'
+   ;; installs *all* packages supported by Spacemacs and never uninstalls them.
+   ;; (default is `used-only')
+   dotspacemacs-install-packages
+   'used-only))
