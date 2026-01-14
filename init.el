f221d3293a7f409472ded1a4387e2015086153e7
lesliebinbin <leslie.huang@basf.com>
2026-01-14 17:07:18 +0800

    disable native extension unless linux

--- init.el
+++ init.el
@@ -1,25 +1,30 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;; This file is loaded by Spacemacs at startup.
 ;; It must be stored in your home directory.
-(add-to-list 'load-path (expand-file-name "private/lisp" dotspacemacs-directory))
+(add-to-list 'load-path
+             (expand-file-name "private/lisp" dotspacemacs-directory))
 (setq user-packages-directory (expand-file-name "private/packages" dotspacemacs-directory))
 (require 'user-custom-functions)
 
 ;;spacemacs layers
-(load-file (expand-file-name "emacs-config/layers.el" dotspacemacs-directory))
+(load-file (expand-file-name "emacs-config/layers.el"
+                             dotspacemacs-directory))
 ;;spacemacs init
 (load-file (expand-file-name "emacs-config/init.el" dotspacemacs-directory))
 ;;spacemacs user-init
-(load-file (expand-file-name "emacs-config/user-init.el" dotspacemacs-directory))
+(load-file (expand-file-name "emacs-config/user-init.el"
+                             dotspacemacs-directory))
 ;;spacemacs user-config
-(load-file (expand-file-name "emacs-config/user-config.el" dotspacemacs-directory))
+(load-file (expand-file-name "emacs-config/user-config.el"
+                             dotspacemacs-directory))
 ;;spacemacs user-env
-(load-file (expand-file-name "emacs-config/user-env.el" dotspacemacs-directory))
+(load-file (expand-file-name "emacs-config/user-env.el"
+                             dotspacemacs-directory))
 ;; native extension
-(load-file (expand-file-name "emacs-config/native-ext.el" dotspacemacs-directory))
-
-
-
+(when (string-match-p "linux"
+                      (symbol-name system-type))
+  (load-file (expand-file-name "emacs-config/native-ext.el"
+                               dotspacemacs-directory)))
 
 
 ;; Do not write anything past this comment. This is where Emacs will
@@ -34,11 +39,11 @@ This function is called at the very end of Spacemacs initialization."
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(ignored-local-variable-values
-     '((js2-basic-offset . 2) (web-mode-indent-style . 2)
-       (web-mode-block-padding . 2) (web-mode-script-padding . 2)
-       (web-mode-style-padding . 2))))
-
+   '(ignored-local-variable-values '((js2-basic-offset . 2)
+                                     (web-mode-indent-style . 2)
+                                     (web-mode-block-padding . 2)
+                                     (web-mode-script-padding . 2)
+                                     (web-mode-style-padding . 2))))
   (custom-set-faces))
 ;; custom-set-faces was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
