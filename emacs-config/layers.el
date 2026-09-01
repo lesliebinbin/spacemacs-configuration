9ca8ebbbd8e519a98c70184460da7de77fa9a85a
Zhibin Huang <lesliebinbin19900129@gmail.com>
2026-09-01 21:08:16 +0800

    make jupyter-eval point to github repo

--- emacs-config/layers.el
+++ emacs-config/layers.el
@@ -158,7 +158,7 @@ This function should only modify configuration layer settings."
    ;; Also include the dependencies as they will not be resolved automatically.
    dotspacemacs-additional-packages
    `(
-     (jupyter-eval :location "/home/lesliebinbinhuang/codings/jupyter-eval")
+     (jupyter-eval :location (recipe :fetcher github :repo "lesliebinbin/emacs-jupyter-eval"))
      simple-httpd
      websocket
      exec-path-from-shell
